73290cfeeba85994606268657ceb39d4cb1b90b9
Jhon Smith(push_back) <jhonsmithworks@gmail.com>
2019-09-24 22:50:08 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+# ----------------------------------------
+# install mpcc
+# ----------------------------------------
+
+# MPD, mpc install
+sudo apt install mpd mpc
+
+# Script install
+sudo mkdir /opt/mpcc/
+sudo cp ./mpcc.py /opt/mpcc
+
+# Register systemd service
+sudo cp ./mpcc.service /etc/systemd/system/
+sudo systemd enable mpcc
+
+# quit message
+echo "reboot for start mpcc."
+
